9f82ba6f613c9d37f636f35188a1e0264fedb18f
Princeodi <110994588+Princeodi@users.noreply.github.com>
2023-07-09 15:44:58 +0100

    0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,17 +1,28 @@
 #!/usr/bin/python3
-from collections import deque
+''' lockbox module '''
+
 
 def canUnlockAll(boxes):
-    n = len(boxes)
-    visited = set()
-    queue = deque([0])
-    while queue:
-        box = queue.popleft()
-        visited.add(box)
+    ''' 
+        CanUnockAll
+        ([boxes]): a list of list
+    '''
 
-        keys = boxes[box]
-        for key in keys:
-            if key < n and key not in visited:
-                queue.append(key)
-
-    return len(visited) == n
+    # initialize a list of unlocked boxes
+    unlocked = [False] * len(boxes)
+    # set the first box oprn
+    unlocked[0] = True
+    # iterate over the boxes
+    for index, box in enumerate(boxes):
+        # check if the box is unlocked
+        if unlocked[index]:
+            # get the keys in the box
+            for index, key in enumerate(box):
+                # set the box with a found key to open
+                if key < len(unlocked):
+                    unlocked[key] = True
+                    # get the keys at the box that has been opened
+                    # set the boxes with the keys to be open
+                    for i in boxes[key]:
+                        unlocked[i] = True
+    return all(unlocked)
